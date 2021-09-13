58c37102c3900a2bcac440ae4df95808bcec9432
markl147 <lohanmark@yahoo.co.uk>
2021-09-13 13:53:54 +0100

    2nd commit

--- src/IteratorExample.java
+++ src/IteratorExample.java
@@ -1,3 +1,35 @@
+import java.util.ArrayList;
+import java.util.ListIterator;
+
 public class IteratorExample {
 
+    public static void main(String[] args) {
+        //Create an Array List
+        ArrayList al = new ArrayList();
+
+        //add elements to the array list
+        al.add("T");
+        al.add("H");
+        al.add("U");
+        al.add("R");
+        al.add("S");
+        al.add("D");
+        al.add("A");
+        al.add("Y");
+
+        System.out.println("Original contents of al: ");
+
+        //create an iterator
+        ListIterator litr = al.listIterator();
+
+        //iterating through the list
+        while(litr.hasNext()) {
+            Object element = litr.next();
+            System.out.println(element + " ");
+
+            //modify the list elements
+            litr.set(element + "+");
+
+        }
+    }
 }
